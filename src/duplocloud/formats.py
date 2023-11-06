2ac1395c19005be50f0e031d319834cb96935aad
Kelly Ferrone <kelly@duplocloud.net>
2023-11-06 16:09:00 -0700

    env outputs are snazzy time

--- src/duplocloud/formats.py
+++ src/duplocloud/formats.py
@@ -1,37 +1,49 @@
 
 def tostring(obj):
-    """Converts a python object to a string.
+  """Converts a python object to a string.
 
-    The default formatter with no fancy pants tranforming.
+  The default formatter with no fancy pants tranforming.
 
-    Args:
-        obj: The python object to convert.
+  Args:
+    obj: The python object to convert.
 
-    Returns:
-        A string representing the object.
-    """
-    return str(obj)
+  Returns:
+    A string representing the object.
+  """
+  return str(obj)
 
 def tojson(obj):
-    """Converts a python object to a JSON string.
+  """Converts a python object to a JSON string.
 
-    Args:
-        obj: The python object to convert.
+  Args:
+    obj: The python object to convert.
 
-    Returns:
-        A JSON string representing the object.
-    """
-    import json
-    return json.dumps(obj)
+  Returns:
+    A JSON string representing the object.
+  """
+  import json
+  return json.dumps(obj)
 
 def toyaml(obj):
-    """Converts a python object to a YAML string.
+  """Converts a python object to a YAML string.
 
-    Args:
-        obj: The python object to convert.
+  Args:
+    obj: The python object to convert.
 
-    Returns:
-        A YAML string representing the object.
-    """
-    import yaml
-    return yaml.dump(obj)
+  Returns:
+    A YAML string representing the object.
+  """
+  import yaml
+  return yaml.dump(obj)
+
+def toenv(obj):
+  """Converts a python object to a .env file format
+  
+  Args:
+    obj: The python object to convert.
+  
+  Returns:
+    A .env file format string representing the object.
+  """
+  envs = [f"{k}={v}" for k, v in obj.items()]
+  return "\n".join(envs)
