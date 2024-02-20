ea9c7ffb38b99062745e5a372b684d821a94a87b
Mick Piereder <mick@duplocloud.net>
2024-02-20 09:05:22 -0700

    Changed 'update' method to use name and task def

--- src/duplo_resource/ecs_service.py
+++ src/duplo_resource/ecs_service.py
@@ -1,12 +1,8 @@
-
-import yaml
 from duplocloud.client import DuploClient
 from duplocloud.resource import DuploTenantResource
 from duplocloud.errors import DuploError
 from duplocloud.commander import Command, Resource
 import duplocloud.args as args
-from models.ecs_service.update_req_body import UpdateEcsServiceRequestBody, CapacityProviderStrategy
-from models.ecs_service.update_config import UpdateEcsServiceConfig
 
 
 @Resource("ecs_service")
@@ -41,65 +37,28 @@ class DuploEcsService(DuploTenantResource):
             raise DuploError(f"ECS Service '{name}' not found", 404)
 
     @Command()
-    def update(
-        self,
-        config_fp:args.FILE,
-    ):
-        """Update an ECS image
+    def update(self, name: args.NAME, task_definition: args.IMAGE):
+        """Update the image for an ECS service.
 
         Args:
-            config_fp (str): The path to the update config file.
+          name (str): The name of the ECS service to update.
+          task_definition (str): The new task definition to use.
         Returns:
-            A message indicating the success of the operation.
+          The updated ECS object.
         Raises:
-            DuploError: If the config file is not found or is invalid."""
-        try:
-            with open(config_fp, 'r') as f:
-                data = yaml.safe_load(f)
-                config = UpdateEcsServiceConfig(**data)
-                body = UpdateEcsServiceRequestBody.from_config(config)
-        except FileNotFoundError:
-            raise DuploError(f"File '{config_fp}' not found", 404)
-        except yaml.YAMLError:
-            raise DuploError(f"Invalid YAML file '{config_fp}'", 400)
-
-
+            DuploError: If the ECS service could not be updated.
+        """
         tenant_id = self.tenant["TenantId"]
-
-        self.duplo.post(
-            f"subscriptions/{tenant_id}/UpdateEcsService",
-            UpdateEcsServiceRequestBody.to_dict(body)
-        )
-
-        return {
-            "message": f"Successfully updated image for ECS service '{config.name}'"
+        ecs_service = {
+            "Name": name,
+            "TaskDefinition": task_definition
         }
 
-    @Command()
-    def create_update_config(self, dest_fp: args.FILE):
-        """Create a sample update config file for ECS service.
+        try:
+            self.duplo.post(
+                f"subscriptions/{tenant_id}/UpdateEcsService",
+                ecs_service)
+        except Exception as e:
+            raise DuploError(f"Failed to update ECS Service '{name}': {e}")
 
-        Args:
-          dest_fp (str): The file path to write the config to.
-        Returns:
-          The path to the created file.
-        """
-        config = UpdateEcsServiceConfig(
-            name="ecs-service-name",
-            task_version="task-def-version",
-            replicas=1,
-            shared_lb=True,
-            dns_prefix="dns-prefix",
-            hc_grace_period=300,
-            old_task_definition_buffer_size=2,
-            capacity_provider_strategy=[
-                CapacityProviderStrategy(
-                    capacity_provider="capacity-provider-name",
-                    weight=1,
-                    base=1
-                )
-            ]
-        )
-        with open(dest_fp, 'w') as f:
-            yaml.dump(UpdateEcsServiceConfig.to_dict(config), f)
-        return dest_fp
+        return self.find(name)
