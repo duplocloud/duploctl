dfbb729bada5e89ca92a6ce5d834d95a2bc59981
Kelly Ferrone <kelly@duplocloud.net>
2024-03-07 08:09:02 -0700

    missed one

--- src/duplocloud/resource.py
+++ src/duplocloud/resource.py
@@ -33,6 +33,6 @@ class DuploTenantResource(DuploResource):
   @property
   def tenant(self):
     if not self._tenant:
-      self._tenant = self.tenant_svc.find(self.duplo.config.tenant)
+      self._tenant = self.tenant_svc.find(self.duplo.tenant)
     return self._tenant
 
