0c3060f943a6ac52556ea8b3371a82d5f533d9ae
Kelly Ferrone <kelly@duplocloud.net>
2024-03-15 12:03:03 -0600

    dependency tests work now

--- src/tests/test_infra.py
+++ src/tests/test_infra.py
@@ -7,55 +7,69 @@ from duplocloud.client import DuploClient
 
 duplo, _ = DuploClient.from_env()
 
-@pytest.mark.integration
-def test_listing_infrastructures():
-  r = duplo.load("infrastructure")
-  try:
-    lot = r("list")
-  except DuploError as e:
-    pytest.fail(f"Failed to list infrastructures: {e}")
-
-@pytest.mark.integration
-def test_finding_infra():
-  r = duplo.load("tenant")
-  try:
-    t = r("find", "default")
-  except DuploError as e:
-    pytest.fail(f"Failed to find default infra: {e}")
-  assert t["AccountName"] == "default"
-
-@pytest.mark.integration
-def test_creating_infrastructures():
-  r = duplo.load("infrastructure")
+@pytest.fixture(scope="module")
+def infra_name():
   inc = random.randint(1, 100)
-  vnum = math.ceil(random.randint(1, 9))
-  name = f"duploctl{inc}"
-  try:
-    r.create({
-      "Name": name,
-      "Accountid": "",
-      "EnableK8Cluster": False,
-      "AzCount": 2,
-      "Vnet": { 
-        "SubnetCidr": 22, 
-        "AddressPrefix": f"11.1{vnum}0.0.0/16"
-      },
-      "Cloud": 0,
-      "OnPremConfig": None,
-      "Region": "us-east-1",
-      "CustomData": [],
-    }, wait=True)
-  except DuploFailedResource as e:
-    pytest.fail(f"Infrastructure is in a failed state: {e}")
-  except DuploError as e:
-    pytest.fail(f"Failed to create tenant: {e}")
-  try:
-    i = r.find(name)
-    assert i["Name"] == name
-  except DuploError as e:
-    pytest.fail(f"Failed to find infrastructure {name}: {e}")
-  try:
-    r("delete", name)
-  except DuploError as e:
-    pytest.fail(f"Failed to delete infrastructure: {e}")
+  return f"duploctl{inc}"
 
+class TestInfra:
+
+  @pytest.mark.integration
+  def test_listing_infrastructures(self):
+    r = duplo.load("infrastructure")
+    try:
+      lot = r("list")
+    except DuploError as e:
+      pytest.fail(f"Failed to list infrastructures: {e}")
+
+  @pytest.mark.integration
+  def test_finding_infra(self):
+    r = duplo.load("tenant")
+    try:
+      t = r("find", "default")
+    except DuploError as e:
+      pytest.fail(f"Failed to find default infra: {e}")
+    assert t["AccountName"] == "default"
+
+  @pytest.mark.integration
+  @pytest.mark.dependency(name = "create_infra")
+  def test_creating_infrastructures(self, infra_name):
+    r = duplo.load("infrastructure")
+    vnum = math.ceil(random.randint(1, 9))
+    name = infra_name
+    print(f"Creating infra '{name}'")
+    try:
+      r.create({
+        "Name": name,
+        "Accountid": "",
+        "EnableK8Cluster": False,
+        "AzCount": 2,
+        "Vnet": { 
+          "SubnetCidr": 22, 
+          "AddressPrefix": f"11.1{vnum}0.0.0/16"
+        },
+        "Cloud": 0,
+        "OnPremConfig": None,
+        "Region": "us-east-1",
+        "CustomData": [],
+      }, wait=True)
+    except DuploFailedResource as e:
+      pytest.fail(f"Infrastructure is in a failed state: {e}")
+    except DuploError as e:
+      pytest.fail(f"Failed to create tenant: {e}")
+    
+  @pytest.mark.integration
+  @pytest.mark.dependency(depends=["create_infra"])
+  def test_find_delete_infra(self, infra_name):
+    r = duplo.load("infrastructure")
+    name = infra_name
+    print(f"Deleting infra '{name}'")
+    try:
+      i = r.find(name)
+      assert i["Name"] == name
+    except DuploError as e:
+      pytest.fail(f"Failed to find infrastructure {name}: {e}")
+    try:
+      r("delete", name)
+    except DuploError as e:
+      pytest.fail(f"Failed to delete infrastructure: {e}")
