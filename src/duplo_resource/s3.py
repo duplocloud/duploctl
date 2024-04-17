859ead436be1f72b2b3d363c158f667cb2ad0df2
Kumar Saurav Sonu <saurav@duplocloud.net>
2024-04-17 22:13:08 +0530

    Implemented review comments

--- src/duplo_resource/s3.py
+++ src/duplo_resource/s3.py
@@ -9,16 +9,5 @@ class DuploS3(DuploTenantResourceV3):
   def __init__(self, duplo: DuploClient):
     super().__init__(duplo, "aws/s3bucket")
 
-  @Command()  
-  def create(self, 
-             body: args.BODY):
-    """Create a job."""
-    name = self.name_from_body(body)
-    print("Running create")
-    super().create(body)
-    return {
-      "message": f"Bucket {name} created successfully."
-    }
-
   def name_from_body(self, body):
     return body["Name"]
