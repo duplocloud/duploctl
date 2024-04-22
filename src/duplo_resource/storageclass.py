539c207d2d854108129b3c4025d9700dde8fa902
Kumar Saurav Sonu <saurav@duplocloud.net>
2024-04-22 22:46:05 +0530

    Added support for storage class CRUD

---
+++ src/duplo_resource/storageclass.py
@@ -0,0 +1,12 @@
+from duplocloud.client import DuploClient
+from duplocloud.resource import DuploTenantResourceV3
+from duplocloud.commander import Resource
+
+
+@Resource("storageclass")
+class DuploStorageClass(DuploTenantResourceV3):
+  def __init__(self, duplo: DuploClient):
+    super().__init__(duplo, "k8s/storageclass")
+
+  def name_from_body(self, body):
+    return body["name"]
