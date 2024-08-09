e0d406213b3a2b4702d2f3bdacf104b670efacbe
akshayd1796 <akshay@duplocloud.net>
2024-08-09 13:03:20 +0530

    updtaed dependency version

--- src/duplocloud/resource.py
+++ src/duplocloud/resource.py
@@ -272,7 +272,6 @@ class DuploTenantResourceV3(DuploResource):
 
   def endpoint(self, name: str=None, path: str=None):
     p = f"v3/subscriptions/{self.tenant_id}/{self.slug}"
-    print("p==", p)
     if name:
       p += f"/{name}"
     if path:
